42dffdd87509584c1e3b4e883c8f54b713744f20
PierreMassanes <pierre.massanes@gmail.com>
2017-01-08 12:03:14 +0100

    GameController not finished

--- www/js/controllers/FirstGameController.js
+++ www/js/controllers/FirstGameController.js
@@ -7,11 +7,11 @@ app.controller('FirstGameCtrl', function($scope, $http, $ionicPopup) {
   // Initialize the rooms and the object/action for the second game
   $scope.initFirstGame = function () {
     $http.get('../json/datav1.json').success(function(data) {
-      $scope.questionList = [];
+      $scope.questionRoomList = [];
       var roomsArray = data.rooms.slice();
-      while($scope.questionList.length < $scope.paramsFirstGame.nb_questions){
+      while($scope.questionRoomList.length < $scope.paramsFirstGame.nb_questions){
         var nb = Math.floor(Math.random()*roomsArray.length);
-        $scope.questionList.push(roomsArray[nb]);
+        $scope.questionRoomList.push(roomsArray[nb]);
         roomsArray.splice(nb,1);
         if(roomsArray.length === 0){
           roomsArray = data.rooms.slice();
@@ -27,28 +27,9 @@ app.controller('FirstGameCtrl', function($scope, $http, $ionicPopup) {
 
   function nextStepFirstGame(){
     $http.get('../json/datav1.json').success(function(data) {
-      $scope.question = $scope.questionList[$scope.remaining_questions - 1];
-      var nb = Math.floor(Math.random()*$scope.question.ref_objects.length);
-      $scope.solution = data.objects.find(function (elmt) {
-        if(elmt.image != undefined && elmt.image == $scope.question.ref_objects[nb])
-          return elmt;
-      });
+      $scope.questionRoom = $scope.questionRoomList[$scope.remaining_questions - 1];
 
-      var roomsArray = data.rooms.slice();
-      var filteredRoomsArray = roomsArray.filter(function (elmt) {
-        return $scope.question.ref_rooms.indexOf(elmt.image) === -1;
-      });
 
-      $scope.badrooms = [];
-      while($scope.badrooms.length < $scope.paramsSecondGame.nb_rooms - 1){
-        nb = Math.floor(Math.random()*filteredRoomsArray.length);
-        $scope.badrooms.push(filteredRoomsArray[nb]);
-        filteredRoomsArray.splice(nb,1);
-      }
-
-      nb = Math.floor(Math.random()*$scope.paramsSecondGame.nb_rooms);
-      $scope.badrooms.splice(nb, 0, $scope.solution)
-      $scope.rooms = $scope.badrooms.slice();
     });
   }
 
